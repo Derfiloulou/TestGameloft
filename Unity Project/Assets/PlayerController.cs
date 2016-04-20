1bb9d12c8d79c2acc1b30302a33777a088aa300d
Derfiloulou <sebas.fouque@gmail.com>
2016-04-20 18:36:30 +0200

    Selection system ok

--- Unity Project/Assets/PlayerController.cs
+++ Unity Project/Assets/PlayerController.cs
@@ -10,12 +10,26 @@ public class PlayerController : MonoBehaviour {
     Vector2 touchEndPosition;
 
     [Header("DEBUG VARIABLES")]
-    Material green;
-    Material red;
+    public Material green;
+    public Material red;
 
+    // Player stats
+    [Header("Player Stats")]
+    public int health = 150;
+    public int attackDamage = 15;
+
+    float meleeRange = 2.12f;
+    float midRange = 3.51f;
+    float longRange = 6.0f;
+
+
+    //Player Animations
+    Animator selfAnimator;
+
+
+    // Swipe and Touch options
     [Header("Inputs Options")]
-    [Space(10)]
-    [Tooltip("Min swipe length to detect a swipe, otherwise it's a touch (1=screenWidth)")]
+    [Tooltip("Min swipe length to detect a swipe, otherwise it's a touch (1=screenHeight)")]
     [Range(0,1.0f)]
     public float minSwipeInputLength = 0.1f;
 
@@ -23,20 +37,21 @@ public class PlayerController : MonoBehaviour {
     [Range(0, 1.0f)]
     public float maxTouchInputTime = 0.5f;
 
-    [Tooltip("Dot product between enemyDirection and swipeDirection (1-dotProduct)")]
-    [Range(0, 0.3f)]
-    public float maxSwipeConeAngleTolerance = 0.05f;
+    [Tooltip("Dot product between enemyDirection and swipeDirection (in dregrees)")]
+    [Range(0, 20f)]
+    public float maxSwipeConeAngleTolerance = 10.0f;
 
-    [Tooltip("Priority of distance (-1), or close to swipe (1) on selection.")]
-    [Range(-1, 1f)]
-    public float priorityOnSelection = 0f;
+    [Tooltip("from which angle difference two ennemies are considered as close as swipe direction (in degrees)")]
+    [Range(0, 5f)]
+    public float angleTolerence = 2.0f;
 
     float timerTouchInput = 0;
 
-    EnemyBehaviour enemySelected;
+    public EnemyBehaviour enemySelected;
 
 	void Start () {
         gm = GameManager.instance;
+        selfAnimator = GetComponentInChildren<Animator>();
         selfTransform = GetComponent<Transform>();
 	}
 	
@@ -58,7 +73,7 @@ public class PlayerController : MonoBehaviour {
             {
                 touchEndPosition = input.position;
                 
-                if (Vector2.Distance(touchStartPosition, touchEndPosition) / Screen.width > minSwipeInputLength) 
+                if (Vector2.Distance(touchStartPosition, touchEndPosition) / Screen.height > minSwipeInputLength) 
                 {
                     SwipeInput((touchEndPosition - touchStartPosition).normalized);
                 }
@@ -99,38 +114,70 @@ public class PlayerController : MonoBehaviour {
             Vector2 enemyPositionOnScreen = gm.cam.WorldToScreenPoint(gm.enemyList[i].GetComponent<Transform>().position);
             Vector2 enemyDirectionOnScreen = (enemyPositionOnScreen - playerPositionOnScreen).normalized;
             float dot = Vector2.Dot(_swipeDirection, enemyDirectionOnScreen);
+            float angleToSwipe = Mathf.Acos(dot) * Mathf.Rad2Deg;
 
             // if enemy is in cone
-            if (dot >= 1 - maxSwipeConeAngleTolerance)
+            if (angleToSwipe <= maxSwipeConeAngleTolerance)
             {
-
+                
                 // if enemy tested is already our selection, ignore it
                 if (gm.enemyList[i] == enemySelected) continue;
 
                 // if is 1st enemy tested, then stock it
-                if (enemyToSelect == null) {
+                if (enemyToSelect == null) 
+                {
                     enemyToSelect = gm.enemyList[i];
                     distToEnemyToSelect = Vector2.Distance(enemyPositionOnScreen, playerPositionOnScreen)/Screen.height;
                     angleToEnemyToSelect = 1 - dot;
+                    continue;
                 }
                
                 else 
                 {
                     // if threat bigger, replace and stock it
-                    if (gm.enemyList[i].threatLevel > enemyToSelect.threatLevel) {
+                    if (gm.enemyList[i].threatLevel > enemyToSelect.threatLevel) 
+                    {
                         enemyToSelect = gm.enemyList[i];
                         distToEnemyToSelect = Vector2.Distance(enemyPositionOnScreen, playerPositionOnScreen) / Screen.height;
                         angleToEnemyToSelect = 1 - dot;
+                        continue;
                     }
+
                     // if threat are equals, test distance and swipe angle
-                    else if(gm.enemyList[i].threatLevel == enemyToSelect.threatLevel){
-                        ///Vector2.Distance(enemyPositionOnScreen, playerPositionOnScreen);
+                    else if(gm.enemyList[i].threatLevel == enemyToSelect.threatLevel)
+                    {
+                        
+                        // if closer to swipe angle than previous (minus tolerance), stock it
+                        if (angleToSwipe <= angleToEnemyToSelect - angleTolerence)
+                        {
+                            enemyToSelect = gm.enemyList[i];
+                            distToEnemyToSelect = Vector2.Distance(enemyPositionOnScreen, playerPositionOnScreen) / Screen.height;
+                            angleToEnemyToSelect = 1 - dot;
+                            continue;
+                        }
+                        
+                        // else if two ennemies are the closest to swipe direction (more or less tolerance), check distance
+                        else if (angleToSwipe < angleToEnemyToSelect + angleTolerence && angleToSwipe > angleToEnemyToSelect - angleTolerence)
+                        {
+                            // take the nearest to player
+                            if (Vector2.Distance(enemyPositionOnScreen, playerPositionOnScreen) / Screen.height < distToEnemyToSelect)
+                            {
+                                enemyToSelect = gm.enemyList[i];
+                                distToEnemyToSelect = Vector2.Distance(enemyPositionOnScreen, playerPositionOnScreen) / Screen.height;
+                                angleToEnemyToSelect = 1 - dot;
+                                continue;
+                            }
+                            else continue;
+                        }
+                      
                     }
                 }
             }
+            // if not in cone
             else continue;
         }
 
+        // if an ennemy was found, then select it
         if (enemyToSelect != null) 
         {
             enemySelected.GetComponent<Renderer>().material = red;
